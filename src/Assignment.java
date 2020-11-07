9c745eecb877112bc57d9a19ee858bd42b8b46cf
Niv <nivgold@post.bgu.ac.il>
2020-11-07 22:15:17 +0200

    initial commit

--- src/Assignment.java
+++ src/Assignment.java
@@ -1,12 +1,9 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.reflect.Array;
-import java.lang.reflect.Type;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
 
 public class Assignment {
